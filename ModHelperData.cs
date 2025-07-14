bdd48be8ea1d117566646278c53b32e7c779969f
Miya <39800618+rspforhp@users.noreply.github.com>
2025-07-15 03:33:14 +0500

    Minor fix, and bump up the version, needs a recompile in the releases tab!

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,8 +4,8 @@ public static class ModHelperData
 {
     public const string Name = "OmegaCrosspathing";
     public const string Description = "Allows you to merge towers together, should be fully compatible with ultimate crosspathing, custom towers, custom heroes, and custom towersets.";
-    public const string Version = "0.0.6";
+    public const string Version = "0.0.7";
     public const string RepoOwner = "GrahamKracker";
     public const string RepoName = "OmegaCrosspathing";
-    public const string WorksOnVersion = "45";
+    public const string WorksOnVersion = "47";
 }
